815ad0d26adaba77344b18522401fb1d5228c0f6
Reid <rlj0713@gmail.com>
2021-01-13 19:46:12 -0500

    commit tree 99ec62a8d3b9a753df6405479b44e659ec38e19d

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+    array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    holder = []
+    holder [0] = array[2]
+    array [2] = array [1]
+    array [1] = holder[0]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|element| element.sub!(element.slice(2), "$")}
+end
+
+def find_a(array)
+    array2 = []
+    array.each {|element| array2 << element if element.start_with?("a")}
+    array2
+end
+
+def sum_array(array)
+    array.inject(:+)
+end
+
+def add_s(array)
+    array.each {|element| element.insert(-1, "s") if element != array[1]}
+end
